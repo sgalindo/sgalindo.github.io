b0db8f3f7077601d6eb3aa0f05a2ad26613397a7
Salvador Galindo <sagalind@ucsc.edu>
2023-08-10 16:15:46 -0700

    Updated saleblazers entry. Removed bio.

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-$(document).ready(function() {
-    /* ---- MOBILE NAVIGATION ---- */
-    $(".burger-icon").on("click", function() {
-        if ($(".main-nav").hasClass("mobile-hide")) {
-            $(".main-nav").removeClass("mobile-hide");
-            $(".main-nav").addClass("mobile-show");
-        } else {
-            $(".main-nav").addClass("mobile-hide");
-            $(".main-nav").removeClass("mobile-show");
-        }
-    });
-    $(".featured h1").hide();
-    $(".main-content").hide();
-});
+//$(document).ready(function() {
+//    /* ---- MOBILE NAVIGATION ---- */
+//    $(".burger-icon").on("click", function() {
+//        if ($(".main-nav").hasClass("mobile-hide")) {
+//            $(".main-nav").removeClass("mobile-hide");
+//            $(".main-nav").addClass("mobile-show");
+//        } else {
+//            $(".main-nav").addClass("mobile-hide");
+//            $(".main-nav").removeClass("mobile-show");
+//        }
+//    });
+//    $(".featured h1").hide();
+//    $(".main-content").hide();
+//});
 
 // $(document).ready(function() {
 //     /* ---- FADE IN ---- */
 //     $('body').css('display', 'none').fadeIn(1000);
 // });
 
-$(window).on("load", function() {
-    /* ---- FADE IN ---- */
-    $(".featured h1").css('color', '#fff').fadeIn(2250);
-    $(".main-content").css('color', '#fff').fadeIn(1000);
-});
+//$(window).on("load", function() {
+//    /* ---- FADE IN ---- */
+//    $(".featured h1").css('color', '#fff').fadeIn(2250);
+//    $(".main-content").css('color', '#fff').fadeIn(1000);
+//});
